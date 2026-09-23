67b59d61285f4896e87e6b3d74b573bb26f52fc2
FEEX <m.lutfee1222@gmail.com>
2026-09-23 10:22:08 +0700

    Update advanced_mult.py

--- cell30/ex03/advanced_mult.py
+++ cell30/ex03/advanced_mult.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-import sys
-
-# ตรวจสอบว่ามีการส่ง argument มาหรือไม่ ถ้ามีให้พิมพ์ none แล้วจบโปรแกรม
-if len(sys.argv) > 1:
-    print("none")
-    sys.exit()
-
 i = 0
 while i <= 10:
     print(f"Table de {i}:", end="")
